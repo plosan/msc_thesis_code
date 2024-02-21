b8d65174973aa39d6e2babb19b26042eff314aca
pedro <lopezsancha.p@gmail.com>
2024-02-21 23:54:14 +0100

    code to compute Frobenius Roots, nu-invariants and compare nu-invariants

---
+++ src/frobenius.py
@@ -0,0 +1,105 @@
+from sage.all import *
+
+def _have_same_length(a, *args):
+    """Checks if argument ``a`` and the elements in ``*args`` have the same len.
+
+    Args:
+        a (list, tuple or set): A list, tuple or set whose length has to be compared to the lengths of elements in ``*args``.
+        *args (tuple of lists, tuples, sets): Elements whose lengths have to be compared to len
+
+    Returns:
+        out (bool): ``True`` if ``len(a)`` is equal to ``len(elem)`` for each ``elem`` in ``*args``, ``False`` otherwise. 
+    """
+    if len(args) == 0:
+        return True
+    ref_len = len(a)
+    for elem in args:
+        if ref_len != len(elem):
+            return False
+    return True
+
+def _check_monomialize(pe, exponents, coefs, bases):
+    if not _have_same_length(exponents, coefs, bases):
+        raise ValueError("Something's up dude")
+    for i in range(len(exponents)):
+        if exponents[i] != coefs[i] * pe + bases[i]:
+            return False
+    return True
+    
+def _monomialize(pe, exponents):
+    """Computes the expression in the standard basis of the R-module F^e R of the monomial with the given exponents.
+    
+    Args:
+        pe (int): Characteristic p to the e-th power, i.e p^e
+        exponents (list or tuple of ints): Exponents of the monomial
+        
+    Returns:
+        coef (tuple of ints): Coefficient in the basis.
+        base (tuple of ints): Exponents of the monomial of the basis element.
+    """
+    coef = tuple([exponent // pe for exponent in exponents])
+    base = tuple([exponent % pe for exponent in exponents])
+    return coef, base
+
+def _monomialize_polynomial(pe, f, gens):
+    expression = dict()
+    f_coefs = f.coefficients()
+    f_exps = f.exponents()
+    for i in range(len(f_coefs)):
+        coef, base_elem = _monomialize(pe, f_exps[i])
+        monomial = f_coefs[i] * _exponents_to_monomial(coef, gens)
+        if base_elem not in expression:
+            expression[base_elem] = [monomial]
+        else:
+            expression[base_elem].append(monomial)
+    return expression
+    
+    # a = list(expression.values())
+    # a = [[] for elem in a]
+
+def _exponents_to_monomial(exponents, gens):
+    f = 1
+    for i in range(len(exponents)):
+        f *= gens[i] ** exponents[i]
+    return f
+
+def frobenius_root(pe, f):
+    gens = f.parent().gens()
+    expression = _monomialize_polynomial(pe, f, gens)
+    expression = list(expression.values())
+    froot_gens = [sum(elem) for elem in expression]
+    return ideal(froot_gens)
+
+def compute_nu_invariants(p, e, f, nu_inv_count = 10):
+    """Computes the nu-invariants of level e of the polynomial f.
+
+    Args:
+        p (int): Characteristic of the ring.
+        e (int): Level of the nu-invariants.
+        f (polynomial): Polynomial whose nu-invariants are to be computed.
+        nu_inv_count (int, optional): Number of nu-invariants to be computed. Defaults to 10.
+    
+    Returns:
+        nu_invariants (list of ints): List with the first ``nu_inv_count`` nu-invariants of level e of f.        
+    """
+
+    nu_invariants = []
+    f_power = f
+    pe = p ** e
+    current_froot = frobenius_root(pe, f_power)
+
+    if not f.parent().ideal(1) <= current_froot:
+        nu_invariants.append(0)
+
+    count = len(nu_invariants)
+    n = 1
+    while count < nu_inv_count:
+        f_power *= f
+        n += 2
+        next_froot = frobenius_root(pe, f_power)
+        if not current_froot <= next_froot:
+            nu_invariants.append(n)
+            current_froot = next_froot
+            count += 1
+            print(count, n)
+    return nu_invariants
---
+++ src/main.py
@@ -0,0 +1,109 @@
+import math
+import numpy as np
+
+from sage.all import *
+from matplotlib import pyplot as plt
+
+import frobenius
+
+def fun1(val, sgn = 1):
+    return sgn * np.power(val, 1.5)
+
+def to_base_n(a, n):
+    
+    coefs = [a % n]
+    while a > 0:
+        a -= coefs[-1]
+        a = int(a / n)
+        coefs.append(a % n)
+    return coefs
+
+def check_base_n(a, n, coefs):
+
+    b = 0
+    power_n = 1
+    for i in range(len(coefs)):
+        b += coefs[i] * power_n
+        power_n *= n
+    
+    return (a - b) == 0
+
+def nice_base_n_expansion(a, n):
+    coefs = to_base_n(a, n)
+    parts = [f'{coefs[i]} * {n}^{i}' for i in range(len(coefs))]
+    out = f"{a} = {' + '.join(parts)}"
+    return out
+
+def plot_nu_invariants(p, e, f, num_nu_inv = 10):
+
+    nu_inv = frobenius.compute_nu_invariants(p, e, f, num_nu_inv)
+
+    x_axis = [i + 1 for i in range(num_nu_inv)]
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, nu_inv, marker = 'o')
+    plt.show()
+
+def compare_nu_invariants(p, e, polynomials, num_nu_inv = 10):
+
+    nu_inv = [frobenius.compute_nu_invariants(p, e, f, num_nu_inv) for f in polynomials]
+    max_nu_inv = max([elem[-1] for elem in nu_inv])
+
+    x_axis = [i + 1 for i in range(num_nu_inv)]
+    fig, ax = plt.subplots()
+    [ax.plot(x_axis, nu_inv_polynomial, marker = 'o') for nu_inv_polynomial in nu_inv]
+    ax.set_xlim([0, num_nu_inv + 1])
+    ax.set_ylim([0, 10 * (math.ceil(max_nu_inv / 10) + 1)])
+    ax.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    # p = 17
+
+    # for d in range(20):
+    #     e = 2 * d + 1
+    #     i = int((p ** e - 2) / 3)
+    #     print(to_base_n(i, p))
+
+    p = 5
+    e = 1
+    R = PolynomialRing(GF(p), 'X, Y')
+    gens = R.gens()
+
+    X, Y = gens
+
+    # def_f = X**3 + Y**2 + X * Y
+
+    f = X**3 + Y**2
+    g = X**2 + Y**7
+    compare_nu_invariants(p, e, (f, g))
+
+    quit()
+    pe = p ** e
+    froot = frobenius.frobenius_root(pe, f)
+
+    print(frobenius.frobenius_root(pe, f))
+    print(frobenius.frobenius_root(pe, def_f))
+
+
+
+    print('Computing nu-invariants')
+
+    compare_deformation_nu_invariants(p, e, f, def_f, num_nu_inv = 10)
+
+    # max_e = 3
+    # num_nu_inv = 10
+    # nu_inv_levels = []
+
+    # for e in range(1, max_e + 1):
+    #     print(f'Computing level {e}...')
+    #     nu_inv_levels.append(frobenius.compute_nu_invariants(p, e, f))
+
+    # x_axis = [i + 1 for i in range(num_nu_inv)]
+    # fig, ax = plt.subplots()
+    # for i in range(max_e):
+    #     ax.plot(x_axis, nu_inv_levels[i], marker = 'o')
+    # plt.show()
+
+    # froot = frobenius
