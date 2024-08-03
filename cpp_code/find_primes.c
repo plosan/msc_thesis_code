6b6bdfcd06d5f082283ef42b9a00b4e7c41946a8
pedro <lopezsancha.p@gmail.com>
2024-08-03 10:25:20 +0200

    feat: C code to compute primes up to a given bound

--- cpp_code/find_primes.c
+++ cpp_code/find_primes.c
@@ -1,38 +1,39 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <cmath>
-#include <climits>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
 
-    int bound = 1e6;
-
-    std::vector<int> primes(1);
+    const int bound = 1e6;                              /* upper bound to compute primes */
+    int *primes = (int*) calloc(bound, sizeof(int));    /* array of primes, it could be more memory-efficient */
+                                                        /* and allocate an array of side log(bound)/bound, but anyway */
+    int size = 1;                                       /* number of primes found */
     primes[0] = 2;
 
+    /* find primes less than bound */
     for(int a = 3; a < bound; ++a) {
         int i = 0;
-        int sz = int(primes.size());
         bool is_prime = true;
-        bool is_less_than_sqrt = true;
-        while(i < sz && is_prime && is_less_than_sqrt) {
+        bool is_bigger_than_sqrt = false;
+        while(i < size && is_prime && !is_bigger_than_sqrt) {
             int p = primes[i];
             is_prime = !(a % p == 0);
-            is_less_than_sqrt = (p < int(sqrt(a)));
+            is_bigger_than_sqrt = (p > (int) floor(sqrt(a)));
             i++;
         }
-        if(is_prime)
-            primes.push_back(a);
-    }
-
-    std::fstream file("primes.txt");
-    if(file.is_open()) {
-        for(int i = 0; i < int(primes.size()); ++i) {
-            file << primes[i] << std::endl;
+        if(is_prime && is_bigger_than_sqrt) {
+            primes[size] = a;
+            size++;
         }
     }
-    file.close();
 
-
+    /* save to file named filename */
+    char filename[256];
+    sprintf(filename, "../data/prime_numbers/primes_%d.txt", bound);
+    FILE *fptr = fopen(filename, "w");
+    for(int i = 0; i < size; ++i) {
+        fprintf(fptr, "%d\n", primes[i]);
+    }
+    fclose(fptr);
 }
